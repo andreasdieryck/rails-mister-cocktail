f3b64e7ba4c874f2c24c730c41d40b88f224dc4b
andreasdieryck <andreas.dieryck@gmail.com>
2017-02-24 20:52:31 +0100

    Commit of the day

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,6 +15,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
+    redirect_to cocktails_path(@cocktail)
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # resources :cocktails
-  get "cocktails", to: "cocktails#index"
-  get "cocktails/new", to: "cocktails#new"
-  get "cocktails/:id", to: "cocktails#show"
-  post "cocktails", to: "cocktails#create"
-  get "cocktails/:id/edit", to: "cocktails#edit"
-  patch "cocktails/:id", to: "cocktails#update"
-  delete "cocktails/:id", to: "cocktails#destroy"
-
+  resources :cocktails
   root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
