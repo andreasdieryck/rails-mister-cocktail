be878e6633c10a2fe2e6ace75a3bf2d28b715d65
andreasdieryck <andreas.dieryck@gmail.com>
2017-02-24 18:40:22 +0100

    commit of the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # resources :cocktails
+  get "cocktails", to: "cocktails#index"
+  get "cocktails/new", to: "cocktails#new"
+  get "cocktails/:id", to: "cocktails#show"
+  post "cocktails", to: "cocktails#create"
+  get "cocktails/:id/edit", to: "cocktails#edit"
+  patch "cocktails/:id", to: "cocktails#update"
+  delete "cocktails/:id", to: "cocktails#destroy"
+
+  root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
